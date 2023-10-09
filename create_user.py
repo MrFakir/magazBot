3a3781d218ce0f8dd78c73205f0efcbad594044a
drvram <fakirsoft@yandex.ru>
2023-10-10 00:57:31 +0500

    add models and create users

--- create_user.py
+++ create_user.py
@@ -1,12 +1,37 @@
-def create_user(personal_number):
-    pass
+from sqlalchemy import insert, Connection, exc, select
+from model import users, engine
 
 
+# from sqlite3 import IntegrityError
 
+
+def create_user(id, first_name, last_name, patronymic, phone_number):
+    stmt = insert(users).values(id=id, first_name=first_name, last_name=last_name, patronymic=patronymic,
+                                phone_number=phone_number)
+    # print(stmt)
+    try:
+        with engine.begin() as conn:  # type: Connection
+            result = conn.execute(statement=stmt)
+            conn.commit()
+    except exc.IntegrityError:
+        # print('Ошибка ключа')
+        # stmt = select(users)
+        # print(stmt)
+        stmt = select(users.c.id, users.c.phone_number).where(users.c.id == id)
+        # print(stmt)
+        with engine.connect() as conn:  # type: Connection
+            result = conn.execute(statement=stmt)
+            # print()
+            row = result.first()
+            # for row in result:
+            #     print(row)
+        print(
+            f'Табельный номер {row[0]} принадлежит номеру телефона +{row[1]}, если эта ошибка, '
+            f'проверь свой введенный табельный, или нажми кнопку "Помощь"')
 
 
 def main():
-    pass
+    create_user(1325, 'Вася', 'Петров', 'Петрович', 79995682244)
 
 
 if main() == '__name__':
--- model.py
+++ model.py
@@ -1,24 +1,33 @@
 # import sqlite3
 import sqlalchemy as db
 
+# engine = db.create_engine('sqlite:///crew_database.db', echo=True)
 engine = db.create_engine('sqlite:///crew_database.db')
 connection = engine.connect()
-metadate = db.MetaData()
+metadate_obj = db.MetaData()
 
-users = db.Table('users', metadate,
+users = db.Table('users', metadate_obj,
                  db.Column('id', db.Integer, primary_key=True),
-                 db.Column('first_name', db.Text),
-                 db.Column('last_name', db.Text),
-                 db.Column('patronymic', db.Text),
+                 db.Column('first_name', db.String),
+                 db.Column('last_name', db.String),
+                 db.Column('patronymic', db.String),
                  db.Column('phone_number', db.Integer),
                  )
-pay = db.Table('pay', metadate,
+pay = db.Table('pay', metadate_obj,
                db.Column('id', db.Integer, primary_key=True),
-               db.Column('flight_number', db.Text),
+               db.Column('flight_number', db.String),
                db.Column('flight_pay', db.Float),
-               db.Column('user_id', db.Integer, db.ForeignKey('users.id'))
+               db.Column('user_id', db.ForeignKey('users.id'), nullable=False)
                )
 
 
+def main():
+    # print(pay.columns)
+    # print()
+    metadate_obj.create_all(engine)
 
-metadate.create_all(engine)
+
+if __name__ == '__main__':
+    main()
+
+
